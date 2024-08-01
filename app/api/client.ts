84fae6751d5084034457d5e7d6653f22a681236a
Yonatan Magier <yonatanmgr@gmail.com>
2024-08-02 00:58:01 +0300

    https

--- app/api/client.ts
+++ app/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "http://export.arxiv.org/api",
+  baseURL: "https://export.arxiv.org/api",
   headers: {
     "Content-Type": "application/json",
   },
