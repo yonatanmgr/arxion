242aad2508f86fb0bec37c084d2621cd1d70a886
Yonatan Magier <yonatanmgr@gmail.com>
2024-08-02 05:31:29 +0300

    refactor: Remove unused dependencies and update components for improved accessibility and responsiveness

--- app/components/Paper/Title.tsx
+++ app/components/Paper/Title.tsx
@@ -1,9 +1,6 @@
-import { BiCopy } from "react-icons/bi";
 import MarkdownBlock from "./Markdown";
-import { useCopyToClipboard } from "usehooks-ts";
 
 const Title = ({ title }: { title: string }) => {
-  const [copiedText, copyToClipboard] = useCopyToClipboard();
   return (
     <MarkdownBlock
       className="sm:text-2xl text-xl font-bold w-full text-center text-balance py-2 sm:py-4 select-none"
