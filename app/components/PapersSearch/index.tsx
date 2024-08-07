26efe197334364a60cd19f6d20c46f5d4a865891
Yonatan Magier <31913495+yonatanmgr@users.noreply.github.com>
2024-08-07 20:57:35 +0300

    Update index.tsx

--- app/components/PapersSearch/index.tsx
+++ app/components/PapersSearch/index.tsx
@@ -107,7 +107,7 @@ const PapersSearch = () => {
         papers={papers}
       />
       {!papers && !isFetching && debouncedSearchQuery && (
-        <div className="flex h-[66dvh] items-center justify-center rounded-md max-sm:h-[33dvh]">
+        <div className="flex h-[66dvh] items-center justify-center rounded-md max-sm:h-[24dvh]">
           <p className="font-mono text-xl italic text-center select-none text-balance text-zinc-500/50 dark:text-zinc-400/50">
             No papers matched the query{" "}
             <span className="not-italic">&quot;{searchQuery}&quot;</span>
