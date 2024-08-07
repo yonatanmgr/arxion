e794fcb965f51c3cd284986973661b8139bceac9
Yonatan Magier <yonatanmgr@gmail.com>
2024-08-07 17:24:16 +0300

    refactor: Add animation in PapersSearch/Results

--- app/components/PapersSearch/Results/index.tsx
+++ app/components/PapersSearch/Results/index.tsx
@@ -1,15 +1,28 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { TArxivEntry } from "@/app/types";
 import ArxivPaper from "../../Paper";
+import { animate, AnimatePresence, stagger } from "framer-motion";
 
 interface ResultsProps {
   papers: TArxivEntry[] | undefined;
 }
 
-const Results = ({ papers }: ResultsProps) => (
-  <section className="flex grow snap-y snap-proximity flex-col gap-4 overflow-y-auto">
-    {papers?.map((entry, index) => <ArxivPaper paper={entry} key={index} />)}
-  </section>
-);
+const Results = ({ papers }: ResultsProps) => {
+  useEffect(() => {
+    papers !== undefined &&
+      papers.length > 0 &&
+      animate("li", { opacity: 1 }, { delay: stagger(0.1) });
+  }, [papers]);
+
+  return (
+    <section className="flex grow flex-col gap-4 overflow-y-auto">
+      <AnimatePresence>
+        {papers?.map((entry, index) => (
+          <ArxivPaper paper={entry} key={index} />
+        ))}
+      </AnimatePresence>
+    </section>
+  );
+};
 
 export default Results;
