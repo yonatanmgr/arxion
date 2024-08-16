824a1a1697712eefc2fb90f0ea1c5f7894d751cb
Yonatan Magier <yonatanmgr@gmail.com>
2024-08-16 15:57:41 +0300

    refactor: Update MathJax configuration to include AMS package

--- app/constants.ts
+++ app/constants.ts
@@ -1,9 +1,9 @@
 import { MathJax3Config } from "better-react-mathjax";
 
 export const MATHJAX_CONFIG: MathJax3Config = {
-  loader: { load: ["[tex]/html", "[tex]/unicode"] },
+  loader: { load: ["[tex]/html", "[tex]/unicode", "[tex]/ams"] },
   tex: {
-    packages: { "[+]": ["html", "unicode"] },
+    packages: { "[+]": ["html", "unicode", "ams"] },
 
     inlineMath: [
       ["$", "$"],
